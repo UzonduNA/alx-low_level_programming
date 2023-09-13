d70a65be9a26c7d9a5b2aee2e8357257395a4e90
UzonduNA <agbaforuzondu@gmail.com>
2023-09-12 18:10:59 -0700

    sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,15 +7,13 @@
  * Return: prints 0 if successful
 */
 
-int main() {
-    printf("Size of char: %lu bytes\n", sizeof(char));
-    printf("Size of short: %lu bytes\n", sizeof(short));
-    printf("Size of int: %lu bytes\n", sizeof(int));
-    printf("Size of long: %lu bytes\n", sizeof(long));
-    printf("Size of long long: %lu bytes\n", sizeof(long long));
-    printf("Size of float: %lu bytes\n", sizeof(float));
-    printf("Size of double: %lu bytes\n", sizeof(double));
-    printf("Size of long double: %lu bytes\n", sizeof(long double));
+int main()
+{
+	printf("Size of char: %lu byte(s)\n", sizeof(char));
+	printf("Size of int: %lu byte(s)\n", sizeof(int));
+	printf("Size of long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of long long int: %lu bytes\n", sizeof(long long int));
+	printf("Size of float: %lu bytes\n", sizeof(float));
     
     return 0;
 }
