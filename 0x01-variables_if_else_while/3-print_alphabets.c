8458411e1a0fa887d0ca593c9df2347b8981f0be
UzonduNA <agbaforuzondu@gmail.com>
2023-10-04 17:12:15 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,19 +11,20 @@ int main(void)
 
 {
 
-char alphabet = 'a' & 'A';
+char alphabet;
 
-	while (alphabet <= 'z')
-	{putchar(alphabet);
-	alphabet++;
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+
+	{
+		putchar(alphabet);
 	}
 
-	while (alphabet <= 'Z')
+	for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
 
 	{
 	putchar(alphabet);
-	alphabet++;
 	}
+
 putchar('\n');
 return (0);
 
